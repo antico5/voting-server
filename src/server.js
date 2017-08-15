b0614fe5ebd9a5adf6173a5ff0cf7fd099d404b9
Armando Andini <armando.andini@hotmail.com>
2017-08-15 15:08:11 -0300

    Rename 'store' action to 'state'.

--- src/server.js
+++ src/server.js
@@ -4,11 +4,11 @@ export default function startServer(store) {
   const io = new Server().attach(8090)
 
   store.subscribe(
-    () => io.emit('store', store.getState().toJS())
+    () => io.emit('state', store.getState().toJS())
   )
 
   io.on('connection', (socket) => {
-    socket.emit('store', store.getState().toJS())
+    socket.emit('state', store.getState().toJS())
     socket.on('action', store.dispatch.bind(store))
   })
 }
